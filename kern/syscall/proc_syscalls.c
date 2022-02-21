f65254692233ce8dbdecd9d2e774fe4997b7ed16
arnavrajiv <62696830+arnavrajiv@users.noreply.github.com>
2022-02-21 01:08:02 -0500

    errors

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -11,6 +11,7 @@
 #include <copyinout.h>
 #include <opt-A1.h>
 #include <limits.h>
+#include <clock.h>
 
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
