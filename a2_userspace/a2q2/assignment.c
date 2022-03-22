55be334c94733c35388d7c3314f7c75b59e3c40c
arnavrajiv <62696830+arnavrajiv@users.noreply.github.com>
2022-03-22 16:50:21 -0400

    userspace q2

--- a2_userspace/a2q2/assignment.c
+++ a2_userspace/a2q2/assignment.c
@@ -1,3 +1,7 @@
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+
 #include "assignment.h"
 
 void
@@ -5,29 +9,48 @@ consume_enter(struct resource *resource)
 {
     // FILL ME IN
 
-    resource->num_consumers += 1;
+    pthread_mutex_lock(&resource->mutex);
+
+    while(resource->num_consumers >= resource->num_producers * resource->ratio) {
+       pthread_cond_wait(&resource->cond, &resource->mutex);
+
+    }
+    resource->num_consumers ++;
+    pthread_cond_signal(&resource->cond);
 }
 
 void
 consume_exit(struct resource *resource)
 {
-    // FILL ME IN
+    assert(resource->num_consumers > 0);
 
-    resource->num_consumers -= 1;
+    resource->num_consumers --;
+    pthread_cond_signal(&resource->cond);
+
+    pthread_mutex_unlock(&resource->mutex);
 }
 
 void
 produce_enter(struct resource *resource)
 {
     // FILL ME IN
+    pthread_mutex_lock(&resource->mutex);
 
+    assert(resource->num_producers >= 0);
+    resource->num_producers += 1;
+    pthread_cond_signal(&resource->cond);
 }
 
 void
 produce_exit(struct resource *resource)
 {
     // FILL ME IN
+    while(resource->num_consumers >= resource->num_producers * resource->ratio) {
+       pthread_cond_wait(&resource->cond, &resource->mutex);
+    }
+    resource->num_producers -= 1;
+    pthread_cond_signal(&resource->cond);
+
+    pthread_mutex_unlock(&resource->mutex);
 
 }
-
-
